78d573871ec1f5bb6550f3dba2659acd002882b3
Bernd Kitt <b.kitt@berndkitt.de>
2023-10-06 19:52:10 +0200

    Header files for global types implemented.

---
+++ common/GlobalTypesBase.h
@@ -0,0 +1,35 @@
+///////////////////////////////////////////////////////////////////////////////
+/// \file GlobalTypesBase.h
+///////////////////////////////////////////////////////////////////////////////
+
+/*
+This file is part of the Robotics Toolbox.
+
+Copyright (C) 2023
+
+Authors: Bernd Kitt (b.kitt@berndkitt.de)
+
+The Robotics Toolbox is free software; you can redistribute it
+and/or modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation; either version 3 of the License,
+or any later version.
+
+The Robotics Toolbox is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+the Robotics Toolbox. If not, see https://www.gnu.org/licenses/.
+*/
+
+#ifndef GLOBALTYPESBASE_H
+#define GLOBALTYPESBASE_H
+
+// base types
+typedef double        T_REAL;   ///< Typedef for floating point values.
+typedef signed long   T_SLONG;  ///< Typedef for signed long values.
+typedef unsigned long T_ULONG;  ///< Typedef for unsigned long values.
+typedef bool          T_BOOL;   ///< Typedef for boolean values.
+
+#endif // GLOBALTYPESBASE_H
