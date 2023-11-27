f03bd852c4a17f98ba8c760cb7f2bb8ce3811e5a
Bernd Kitt <b.kitt@berndkitt.de>
2023-11-27 19:21:01 +0100

    Dataset reader base implemented.

---
+++ common/DatasetReaderBase.cpp
@@ -0,0 +1,82 @@
+///////////////////////////////////////////////////////////////////////////////
+/// \file DatasetReaderBase.cpp
+///////////////////////////////////////////////////////////////////////////////
+
+/*
+This file is part of the Robotics Toolbox.
+
+Copyright (C) 2023
+
+Authors: Bernd Kitt (b.kitt@berndkitt.de)
+
+The Robotics Toolbox is free software; you can redistribute it
+and/or modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation; either version 3 of the License,
+or any later version.
+
+The Robotics Toolbox is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+the Robotics Toolbox. If not, see https://www.gnu.org/licenses/.
+*/
+
+#include "DatasetReaderBase.h"
+
+DatasetReaderBase::DatasetReaderBase(const std::string BaseDirectory,
+                                     const std::string SequenceName)
+{
+    // set internal attributes
+    m_BaseDirectory = BaseDirectory;
+    m_SequenceName  = SequenceName;
+}
+
+DatasetReaderBase::~DatasetReaderBase()
+{
+
+}
+
+void DatasetReaderBase::ExtractFilesInDirectory(const std::filesystem::path     Path,
+                                                const std::string               FileBasename,
+                                                const std::string               FileExtension,
+                                                      std::vector<std::string>& ListFilenamesWithPath,
+                                                      uint64&                   NumberOfFilesFound)
+{
+    // check whether the provided path exists or not
+    if(!exists(Path) || !is_directory(Path))
+    {
+        throw std::invalid_argument("Directory " + Path.string() + " does not exist.");
+    }
+
+    // get files in the directory (i.e. iterate over all files)
+    std::filesystem::directory_iterator DirectoryIterator(Path);
+
+    for(auto& p: DirectoryIterator)
+    {
+        // get filename of current file
+        const std::string Filename {p.path().filename()};
+
+        // check whether the name of the current file starts with specified basename and ends with specified extension
+        const boolean HasCorrectBasename  {Filename.find(FileBasename) == 0};
+        const boolean HasCorrectExtension {Filename.find(FileExtension) == Filename.length() - FileExtension.length()};
+
+        if(HasCorrectBasename && HasCorrectExtension)
+        {
+            ListFilenamesWithPath.push_back(Path / p.path().filename());
+        }
+    }
+
+    // sort filenames found
+    std::sort(ListFilenamesWithPath.begin(),ListFilenamesWithPath.end());
+
+    // get number of files found
+    NumberOfFilesFound = ListFilenamesWithPath.size();
+
+    // check whether files were found in the specified directory or not
+    if(NumberOfFilesFound == 0)
+    {
+        throw std::invalid_argument("Directory " + Path.string() + " does not contain files matching the conditions.");
+    }
+}
---
+++ common/DatasetReaderBase.h
@@ -0,0 +1,96 @@
+///////////////////////////////////////////////////////////////////////////////
+/// \file DatasetReaderBase.h
+///////////////////////////////////////////////////////////////////////////////
+
+/*
+This file is part of the Robotics Toolbox.
+
+Copyright (C) 2023
+
+Authors: Bernd Kitt (b.kitt@berndkitt.de)
+
+The Robotics Toolbox is free software; you can redistribute it
+and/or modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation; either version 3 of the License,
+or any later version.
+
+The Robotics Toolbox is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+the Robotics Toolbox. If not, see https://www.gnu.org/licenses/.
+*/
+
+#ifndef DATASETREADERBASE_H
+#define DATASETREADERBASE_H
+
+#include <filesystem>
+
+#include "GlobalTypesDerived.h"
+
+///////////////////////////////////////////////////////////////////////////////
+/// \class DatasetReaderBase
+///
+/// \brief Base class for different kinds of dataset readers.
+///////////////////////////////////////////////////////////////////////////////
+class DatasetReaderBase
+{
+protected: // protected attributes
+    std::filesystem::path    m_BaseDirectory;                           ///< Absolute path to the base directory of the dataset.
+    std::filesystem::path    m_SequenceName;                            ///< Relative path to the sequence (w.r.t. the base directory).
+    std::filesystem::path    m_RelativePathImagesStereoLeft;            ///< Relative path to the left stereo camera images (w.r.t. the sequence).
+    std::filesystem::path    m_RelativePathImagesStereoRight;           ///< Relative path to the right stereo camera images (w.r.t. the sequence).
+    std::filesystem::path    m_AbsolutePathImagesStereoLeft;            ///< Absolute path to the left stereo camera images.
+    std::filesystem::path    m_AbsolutePathImagesStereoRight;           ///< Absolute path to the right stereo camera images.
+    std::string              m_FileBasenameImagesStereoLeft;            ///< Basename of the left stereo camera images.
+    std::string              m_FileBasenameImagesStereoRight;           ///< Basename of the right stereo camera images.
+    std::string              m_FileExtensionImagesStereoLeft;           ///< File extension of the left stereo camera images.
+    std::string              m_FileExtensionImagesStereoRight;          ///< File extension of the right stereo camera images.
+    uint64                   m_NumberOfImagesStereoLeft;                ///< Number of left stereo camera images.
+    uint64                   m_NumberOfImagesStereoRight;               ///< Number of right stereo camera images.
+    std::vector<std::string> m_FilenamesWithPathImagesStereoLeft;       ///< List of filenames including the absolute path to the left stereo camera images.
+    std::vector<std::string> m_FilenamesWithPathImagesStereoRight;      ///< List of filenames including the absolute path to the right stereo camera images.
+    std::filesystem::path    m_FilenameTimestampsImagesStereoLeft;      ///< Name of the file containing the timestamps of the left stereo camera images.
+    std::filesystem::path    m_FilenameTimestampsImagesStereoRight;     ///< Name of the file containing the timestamps of the right stereo camera images.
+    std::filesystem::path    m_AbsolutePathTimestampsImagesStereoLeft;  ///< Absolute path to the file containing the timestamps of the left stereo camera images.
+    std::filesystem::path    m_AbsolutePathTimestampsImagesStereoRight; ///< Absolute path to the file containing the timestamps of the right stereo camera images.
+    ListUInt64               m_TimestampsImagesStereoLeftNanoseconds;   ///< List of timestamps of the left stereo camera images (in nanoseconds).
+    ListUInt64               m_TimestampsImagesStereoRightNanoseconds;  ///< List of timestamps of the right stereo camera images (in nanoseconds).
+    uint64                   m_NumberOfTimestampsStereoLeft;            ///< Number of left stereo camera images.
+    uint64                   m_NumberOfTimestampsStereoRight;           ///< Number of right stereo camera images.
+
+public: // public methods
+    ///////////////////////////////////////////////////////////////////////////////
+    /// \brief     Constructor.
+    ///
+    /// \param[in] BaseDirectory Absolute path to the base directory of the dataset.
+    /// \param[in] SequenceName  Relative path to the sequence (w.r.t. the base directory).
+    ///////////////////////////////////////////////////////////////////////////////
+    DatasetReaderBase(const std::string BaseDirectory,
+                      const std::string SequenceName);
+
+    ///////////////////////////////////////////////////////////////////////////////
+    /// \brief Destructor.
+    ///////////////////////////////////////////////////////////////////////////////
+    virtual ~DatasetReaderBase();
+
+protected: // protected methods
+    ///////////////////////////////////////////////////////////////////////////////
+    /// \brief      Extracts the list of files in a directory.
+    ///
+    /// \param[in]  Path                  Absolute path to the files.
+    /// \param[in]  FileBasename          Basename of the files to be extracted.
+    /// \param[in]  FileExtension         Extension of the files to be extracted.
+    /// \param[out] ListFilenamesWithPath List of filenames found, including the absolute path.
+    /// \param[out] NumberOfFilesFound    Number of files found.
+    ///////////////////////////////////////////////////////////////////////////////
+    void ExtractFilesInDirectory(const std::filesystem::path     Path,
+                                 const std::string               FileBasename,
+                                 const std::string               FileExtension,
+                                       std::vector<std::string>& ListFilenamesWithPath,
+                                       uint64&                   NumberOfFilesFound);
+};
+
+#endif // DATASETREADERBASE_H
