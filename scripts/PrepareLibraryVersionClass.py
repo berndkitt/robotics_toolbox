f4d93b7866a9cfe0c84fb784882781304b385302
Bernd Kitt <b.kitt@berndkitt.de>
2024-02-06 20:03:12 +0100

    Preparation of the unit tests of the library version class implemented.

--- scripts/PrepareLibraryVersionClass.py
+++ scripts/PrepareLibraryVersionClass.py
@@ -1,8 +1,8 @@
 """
 Script to prepare the version class for a library.
 
-The script copies the templates for the version class into the library's directory and replaces the
-placeholders by proper values.
+The script copies the templates for the version class as well as the corresponding unit test file
+into the library's directory and replaces the placeholders by proper values.
 """
 
 import argparse
@@ -55,24 +55,31 @@ if __name__ == "__main__":
     search_string_library_abbreviation_uppercase = "${LIB_ABBREVIATION_UPPERCASE}"
     filename_source_header                       = "LIBVersion.h"
     filename_source_source                       = "LIBVersion.cpp"
+    filename_source_unit_tests                   = "Test_LIBVersion.cpp"
 
     # create source filenames
-    filename_with_path_source_header = f"{args.directory_template_files}/{filename_source_header}"
-    filename_with_path_source_source = f"{args.directory_template_files}/{filename_source_source}"
+    filename_with_path_source_header     = f"{args.directory_template_files}/{filename_source_header}"
+    filename_with_path_source_source     = f"{args.directory_template_files}/{filename_source_source}"
+    filename_with_path_source_unit_tests = f"{args.directory_template_files}/{filename_source_unit_tests}"
 
     # create target filenames
-    filename_target_header = f"LIB{args.library_abbreviation.upper()}Version.h"
-    filename_target_source = f"LIB{args.library_abbreviation.upper()}Version.cpp"
+    filename_target_header     = f"LIB{args.library_abbreviation.upper()}Version.h"
+    filename_target_source     = f"LIB{args.library_abbreviation.upper()}Version.cpp"
+    filename_target_unit_tests = f"Test_LIB{args.library_abbreviation.upper()}Version.cpp"
 
-    filename_with_path_target_header = f"{args.directory_library}/source_code/include/{filename_target_header}"
-    filename_with_path_target_source = f"{args.directory_library}/source_code/src/{filename_target_source}"
+    filename_with_path_target_header     = f"{args.directory_library}/source_code/include/{filename_target_header}"
+    filename_with_path_target_source     = f"{args.directory_library}/source_code/src/{filename_target_source}"
+    filename_with_path_target_unit_tests = f"{args.directory_library}/testing/google_test/source_code/{filename_target_unit_tests}"
 
     # copy template files to source library directory
     shutil.copy2(filename_with_path_source_header, filename_with_path_target_header)
     shutil.copy2(filename_with_path_source_source, filename_with_path_target_source)
+    shutil.copy2(filename_with_path_source_unit_tests, filename_with_path_target_unit_tests)
 
     # replace information in target files
     replace_library_name(filename_with_path_target_header, search_string_library_abbreviation, args.library_abbreviation)
     replace_library_name(filename_with_path_target_header, search_string_library_abbreviation_uppercase, args.library_abbreviation.upper())
     replace_library_name(filename_with_path_target_source, search_string_library_abbreviation, args.library_abbreviation)
     replace_library_name(filename_with_path_target_source, search_string_library_abbreviation_uppercase, args.library_abbreviation.upper())
+    replace_library_name(filename_with_path_target_unit_tests, search_string_library_abbreviation, args.library_abbreviation)
+    replace_library_name(filename_with_path_target_unit_tests, search_string_library_abbreviation_uppercase, args.library_abbreviation.upper())
