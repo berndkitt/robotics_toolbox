79b251c32f32e872b1a8e50e2acf0d8ac7499cb4
Bernd Kitt <b.kitt@berndkitt.de>
2024-02-18 18:49:02 +0100

    Metrix++ analyzer implemented.

---
+++ scripts/metrixplusplus/MetrixPlusPlusAnalyzer.py
@@ -0,0 +1,31 @@
+import os
+
+from file_collector import FileCollector
+
+
+class MetrixPlusPlusAnalyzer:
+    def __init__(self,
+                 base_directory:     str,
+                 configuration_json: str) -> None:
+        # set attributes
+        self._base_directory              = base_directory
+        self._entity_name                 = os.path.basename(os.path.normpath(self._base_directory))
+        self._directory_results           = f"{self._base_directory}testing/metrixplusplus/results/"
+        self._filename_database_with_path = f"{self._directory_results}metrixplusplus.db"
+        self._filename_results_with_path  = f"{self._directory_results}{self._entity_name}_results.csv"
+
+        # get list of files to check
+        files                         = FileCollector.FileCollector(configuration_json, base_directory)
+        self._list_of_files_as_string = files.get_list_of_files_as_string()
+
+    # function to perform the analysis
+    def analyze(self) -> None:
+        # create directory for the results
+        os.makedirs(self._directory_results, exist_ok=True)
+
+        # run analysis for defined entity
+        command_collect = f"python3 /usr/local/bin/metrix++ collect --std.code.filelines.code --std.code.lines.code --std.code.complexity.cyclomatic --std.code.complexity.maxindent --std.code.maintindex.simple --db-file={self._filename_database_with_path} -- {self._list_of_files_as_string}"
+        command_export  = f"python3 /usr/local/bin/metrix++ export --db-file={self._filename_database_with_path} > {self._filename_results_with_path}"
+
+        os.system(command_collect)
+        os.system(command_export)
