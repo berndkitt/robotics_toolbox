aad543070b4bc33ffdeeda5ea63aa4b36dc5afa8
Bernd Kitt <b.kitt@berndkitt.de>
2024-03-09 19:27:47 +0100

    Test report generator for Cppcheck implemented.

--- scripts/RunCppcheck.py
+++ scripts/RunCppcheck.py
@@ -4,14 +4,18 @@ Script to run Cppcheck.
 The script runs Cppcheck on a list of files and parses the analysis results.
 
 Args:
-    base_directory (str):       Base directory.
-    configuration_xml (str):    Filename of the configuration including its path.
-    check_configuration (bool): Flag to check the Cppcheck configuration.
+    base_directory (str):              Base directory.
+    configuration_xml (str):           Filename of the configuration including its path.
+    check_configuration (bool):        Flag to check the Cppcheck configuration.
+    cppcheck_configuration_json (str): Filename of the Cppcheck configuration including its path.
+    filename_report (str):             Filename of the Cppcheck test report including its path.
 """
 import argparse
 
 from cppcheck import CppcheckAnalyzer
 from cppcheck import CppcheckResultsParser
+
+from test_report_generators import TestReportGeneratorCppcheck
 
 
 # run script
@@ -32,6 +36,14 @@ if __name__ == "__main__":
                         default=False,
                         action="store_true",
                         help="Flag to check the Cppcheck configuration.")
+    parser.add_argument("--cppcheck_configuration_json",
+                        required=False,
+                        default="./cppcheck_configuration.json",
+                        help="Filename of the Cppcheck configuration including its path.")
+    parser.add_argument("--filename_report",
+                        required=False,
+                        default="",
+                        help="Filename of the Cppcheck test report including its path.")
 
     args = parser.parse_args()
 
@@ -44,3 +56,16 @@ if __name__ == "__main__":
 
     # parse Cppcheck results
     cppcheck_results_parser.parse_results()
+
+    # create Cppcheck test report generator
+    test_report_generator_cppcheck = TestReportGeneratorCppcheck.TestReportGeneratorCppcheck(args.cppcheck_configuration_json, cppcheck_results_parser.get_error_list())
+
+    # print test report on console
+    test_report_generator_cppcheck.print_report_on_console(True)
+
+    # write test report to file
+    if args.filename_report:
+        test_report_generator_cppcheck.write_report_to_file(args.filename_report, True)
+
+    # return number of failed tests as exit code
+    exit(test_report_generator_cppcheck.get_number_of_failed_tests())
--- scripts/cppcheck/CppcheckErrorList.py
+++ scripts/cppcheck/CppcheckErrorList.py
@@ -21,6 +21,27 @@ class CppcheckErrorList:
         self.__number_of_errors += 1
         self.__error_list.append(error)
 
+    def get_number_of_errors_by_severity(self,
+                                         cppcheck_severity: CppcheckSeverity.CppcheckSeverity) -> int:
+        """
+        Getter for the number of errors for a given Cppcheck severity.
+
+        Args:
+            cppcheck_severity (CppcheckSeverity.CppcheckSeverity): Cppcheck severity.
+
+        Returns:
+            int: Number of errors for the given Cppcheck severity.
+        """
+        number_of_errors = 0
+
+        for current_error in self.__error_list:
+            current_severity = current_error.get_severity()
+
+            if current_severity == cppcheck_severity:
+                number_of_errors += 1
+
+        return number_of_errors
+
     def print_all_errors(self) -> None:
         """Print all errors."""
         for current_error in self.__error_list:
